398ab1fd759c26d00eeb4cdc826057ae3d736eb3
Satch <grantsrb@gmail.com>
2021-11-24 16:01:06 -0800

    add manual testing system

---
+++ manual_test.py
@@ -0,0 +1,31 @@
+import gordongames
+from gordongames.envs.ggames.constants import *
+import gym
+
+if __name__=="__main__":
+    args = {
+        "grid_size": (10,10),
+        "pixel_density": 1,
+        "targ_range": (1,4),
+    }
+    env = gym.make("gordongames-v0", **args)
+
+    done = False
+    rew = 0
+    obs = env.reset()
+    env.render()
+    key = ""
+    action = "w"
+    while key != "q":
+        key = input("action: ")
+        if key   == "w": action = UP
+        elif key == "d": action = RIGHT
+        elif key == "s": action = DOWN
+        elif key == "a": action = LEFT
+        elif key == "g": action = 5
+        obs, rew, done, info = env.step(action)
+        print("rew:", rew)
+        print("done:", done)
+        env.render()
+        if done:
+            env.reset()
