375641477446e23341b1b4d52d64caf0f53c40a4
Satch <grantsrb@gmail.com>
2022-01-05 22:40:44 -0800

    adapt tests for grid changes, did not add new tests for new functionality

--- tests/grid_tests.py
+++ tests/grid_tests.py
@@ -1,6 +1,6 @@
-from grid import Grid
+from mathblocks.blocks.grid import Grid
 import matplotlib.pyplot as plt
-from constants import PLAYER, TARG, PILE, ITEM, DIVIDER, BUTTON, OBJECT_TYPES, STAY, UP, RIGHT, DOWN, LEFT, DIRECTIONS, COLORS, EVENTS, STEP, BUTTON, FULL, DEFAULT
+from mathblocks.blocks.constants import PLAYER, TARG, PILE, ITEM, DIVIDER, BUTTON, OBJECT_TYPES, STAY, UP, RIGHT, DOWN, LEFT, DIRECTIONS, COLORS, EVENTS, STEP, BUTTON, FULL, DEFAULT
 import numpy as np
 
 if __name__=="__main__":
@@ -10,7 +10,7 @@ if __name__=="__main__":
     assert grid.pixel_shape == (31,31)
     assert grid.density == 1
     assert grid.is_divided == False
-    assert grid.middle_row == 16
+    assert grid.middle_row == 15
     assert grid.units2pixels((10,15)) == (10,15)
     assert grid.pixels2units((10,15)) == (10,15)
     assert grid.is_inbounds((2,3), (2,4))
@@ -48,25 +48,27 @@ if __name__=="__main__":
     assert grid.pixel_shape == (31,31)
     assert grid.density == 1
     assert grid.is_divided == True
-    assert grid.middle_row == 16
+    assert grid.middle_row == 15
     assert grid.is_inhalfbounds((0,15), (1,1))
     assert grid.is_inhalfbounds((1,5) , (1,1))
     assert grid.is_playable((1,5), (1,1))
     assert not grid.is_playable((17,0), (1,1))
     assert not grid.is_playable((16,0), (1,1))
     assert grid.is_playable((4,17), (1,1))
+    plt.imshow(grid.grid)
+    plt.show()
 
     grid = Grid(30, pixel_density=1, divide=True)
     plt.imshow(grid.grid)
     plt.show()
     arr = np.zeros((30,30))
-    arr[16,:] = COLORS[DIVIDER]
+    arr[grid.middle_row,:] = COLORS[DIVIDER]
     assert np.array_equal(grid.grid, arr)
     assert grid.shape == (30,30)
     assert grid.pixel_shape == (30,30)
     assert grid.density == 1
     assert grid.is_divided == True
-    assert grid.middle_row == 16
+    assert grid.middle_row == 15
 
     grid = Grid(31, pixel_density=10, divide=False)
     assert np.array_equal(grid.grid, np.zeros((310,310)))
@@ -74,7 +76,7 @@ if __name__=="__main__":
     assert grid.pixel_shape == (310,310)
     assert grid.density == 10
     assert grid.is_divided == False
-    assert grid.middle_row == 16
+    assert grid.middle_row == 15
     assert grid.units2pixels((10,15)) == (100,150)
     assert grid.pixels2units((10,15)) == (1,1)
     assert grid.pixels2units((105,9)) == (10,0)
@@ -85,7 +87,7 @@ if __name__=="__main__":
     assert grid.shape == (31,31)
     assert grid.pixel_shape == (310,310)
     assert grid.density == 10
-    assert grid.middle_row == 16
+    assert grid.middle_row == 15
 
     grid.draw((1,2), (1,1), color=1)
     assert grid.grid[10,20] == 1
@@ -98,7 +100,7 @@ if __name__=="__main__":
     grid.clear_unit((11,3))
     grid.clear_unit((11,4))
     assert not np.array_equal(grid.grid, np.zeros(grid.pixel_shape))
-    assert np.array_equal(grid.grid[:159,:], np.zeros((159,310)))
+    assert np.array_equal(grid.grid[:149,:], np.zeros((149,310)))
 
     grid.slice_draw((1,2), (3,5), color=1)
     grid.slice_draw((1,7), (1,9), color=1)
